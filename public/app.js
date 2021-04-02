43000b35eddae3dd5c61d5be6f4875f7af82ce17
Adrian Ross <adriantoddross@gmail.com>
2021-04-02 10:50:45 -0400

    Let users sign in with Google

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-console.log(firebase);
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
