18e43817173d64ff96de679c5e48ef92ca8aa93b
Adrian Ross <adriantoddross@gmail.com>
2021-04-02 10:59:26 -0400

    Display user info on login, need to resolve errors

--- public/app.js
+++ public/app.js
@@ -8,3 +8,21 @@ const provider = new firebase.auth.GoogleAuthProvider();
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 signOutBtn.onclick = () => auth.signOut();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+const userDetails = document.getElementById("userDetails");
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // Signed In
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}! </h3> <p>User ID: ${user.uid}</p>`;
+  } else {
+    // Not Signed In
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML;
+  }
+});
