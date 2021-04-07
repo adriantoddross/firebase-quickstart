cfaf7c219d6a575ab57b2544586eb7544bb272fc
Adrian Ross <adriantoddross@gmail.com>
2021-04-07 10:27:00 -0400

    Test createThing and receive error

--- public/app.js
+++ public/app.js
@@ -13,12 +13,32 @@ const whenSignedIn = document.getElementById("whenSignedIn");
 const whenSignedOut = document.getElementById("whenSignedOut");
 const userDetails = document.getElementById("userDetails");
 
+const createThing = document.getElementById("createThing");
+const thingsList = document.getElementById("thingsList");
+
+const db = firebase.firestore();
+let thingsRef;
+let unsubscribe;
+
 auth.onAuthStateChanged((user) => {
   if (user) {
     // Signed In
     whenSignedIn.hidden = false;
     whenSignedOut.hidden = true;
     userDetails.innerHTML = `<h3>Hello ${user.displayName}! </h3> <p>User ID: ${user.uid}</p>`;
+
+    // Database reference
+    thingsRef = db.collection("things");
+
+    createThing.onclick = () => {
+      const { serverTimestamp } = firebase.firestore.FieldValue;
+
+      thingsRef.add({
+        uid: user.uid,
+        name: "random item!!!",
+        createdAt: serverTimestamp(),
+      });
+    };
   } else {
     // Not Signed In
     whenSignedIn.hidden = true;
